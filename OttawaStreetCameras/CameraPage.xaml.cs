6aa3cd7fc0f655be0a6a1b53c4ed475aea18aebd
Jason <tehblackboy@gmail.com>
2016-12-13 02:27:35 -0500

    made some progress

--- OttawaStreetCameras/CameraPage.xaml.cs
+++ OttawaStreetCameras/CameraPage.xaml.cs
@@ -1,16 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
+using System.Net;
+using System.Net.Http;
 using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading;
+using System.Threading.Tasks;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.Storage.Streams;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
 using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Imaging;
 using Windows.UI.Xaml.Navigation;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
@@ -19,9 +26,39 @@ namespace OttawaStreetCameras {
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
+
     public sealed partial class CameraPage : Page {
+        BitmapImage bmp = new BitmapImage();
+        string[] param;
         public CameraPage() {
             this.InitializeComponent();
+        }
+        public async void getImage(string[] id) {
+            Uri url = new Uri("https://traffic.ottawa.ca/map/camera?id=" + id[0], UriKind.Absolute);
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+            request.Headers["Cookie"] = id[1];
+
+            HttpWebResponse webResponse = (HttpWebResponse) await request.GetResponseAsync();
+            Stream stream = webResponse.GetResponseStream();
+
+            bmp = new BitmapImage(url);
+            bmp.CreateOptions = BitmapCreateOptions.IgnoreImageCache;
+            image.Source = bmp;
+            Debug.WriteLine(url);
+            await Task.Delay(TimeSpan.FromSeconds(1));
+            getImage(id);
+
+        }
+        public void setImage(BitmapImage bmp) {
+            //image.Source = RandomAccessStreamReference.CreateFromUri(new Uri(url));
+        }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e) {
+            base.OnNavigatedTo(e);
+
+            string[] param = (string[])e.Parameter;
+            this.param = param;
+            getImage(param);
         }
     }
 }
--- OttawaStreetCameras/MainPage.xaml.cs
+++ OttawaStreetCameras/MainPage.xaml.cs
@@ -1,25 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Diagnostics;
-using System.IO;
-using System.Linq;
 using System.Net;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.ApplicationModel.Core;
 using Windows.Data.Json;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
 using Windows.Storage;
-using Windows.UI.Core;
-using Windows.UI.ViewManagement;
-using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Media.Imaging;
-using Windows.UI.Xaml.Navigation;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -29,19 +13,18 @@ namespace OttawaStreetCameras {
     /// </summary>
     public sealed partial class MainPage : Page {
         private List<Camera> listOfCameras = new List<Camera>();
-        static string SESSION_ID = "";
+        public static string SESSION_ID;
+
         public MainPage() {
             this.InitializeComponent();
             getFile();
             getSessionId();
-            //WebRequest request = WebRequest.Create("http://www.contoso.com/");
-
-
         }
+
         public async void getSessionId() {
             HttpWebRequest request = (HttpWebRequest)WebRequest.Create("https://traffic.ottawa.ca/map");
             WebResponse response = await request.GetResponseAsync();
-            Debug.WriteLine(response.Headers["Set-Cookie"]);
+            SESSION_ID =  response.Headers["Set-Cookie"];
         }
         public async void getFile() {
             string filename = "ints.json";
@@ -62,23 +45,11 @@ namespace OttawaStreetCameras {
         }
 
         private void listView_SelectionChanged(object sender, SelectionChangedEventArgs e) {
-            loadImage(((Camera)e.AddedItems[0]).id);
-            
-
-        }
-        public async void loadImage(string id){
-            Uri url = new Uri("https://traffic.ottawa.ca/map/camera?id=" + id);
-            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
-            Cookie cookie = new Cookie("Cookie", SESSION_ID);
-            request.CookieContainer = new CookieContainer();
-            request.CookieContainer.Add(url, cookie);
-            System.Net.WebResponse response = await request.GetResponseAsync();
-            System.IO.Stream responseStream = response.GetResponseStream();
-            update(new BitmapImage(url));
-        }
-        public void update(BitmapImage bmp) {
-            image.Source = bmp;
-            //lol
+            string id = ((Camera)e.AddedItems[0]).id;
+            var param = new string[2];
+            param[0] = id;
+            param[1] = SESSION_ID;
+            this.Frame.Navigate(typeof(CameraPage), param);
         }
     }
 }
